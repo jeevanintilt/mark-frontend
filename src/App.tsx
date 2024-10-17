6c134cd711322d8d32dfebdbc768ea17065f94f3
jeevanintilt <jeevan.intilt@gmail.com>
2024-10-17 17:17:21 +0530

    loginform markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
+import { LoginForm } from "@/components/LoginForm"
 
 const App = () => {
   return (
-  <div className="flex items-center justify-center h-screen w-full">
-    <Button>Click Me</Button>
+  // <div className="flex items-center justify-center h-screen w-full">
+  <div className="flex flex-col items-center justify-center h-screen w-full">
+    {/* <Button variant='default'>Click Me</Button> */}
+    <LoginForm />
   </div>  
   );
 };
